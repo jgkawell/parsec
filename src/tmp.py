676b48a9ba46d5f38ed723ed54aac298bf90b22e
Jack Kawell <jgkawell@outlook.com>
2020-05-06 11:55:24 -0600

    Removed extra files

--- src/tmp.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-test_type = "tree-nlp"
-now = datetime.now()
-timestr = now.strftime("_%m-%d-%Y_%H-%M-%S")
-file_name = '/' + test_type + timestr + '.csv'
-print(file_name)
